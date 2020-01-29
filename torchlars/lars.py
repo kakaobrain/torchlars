4d43191e812853f2df28753431b7aa1794f5ec7b
chois <rightx2@gmail.com>
2020-01-29 18:27:53 +0900

    Update LARS: override __copy__ and __deepcopy__

--- torchlars/lars.py
+++ torchlars/lars.py
@@ -50,6 +50,21 @@ class LARS(Optimizer):
         self.trust_coef = trust_coef
         self.adaptive_lr = torch.ones([])
 
+    def __copy__(self):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
+
+    def __deepcopy__(self, memo):
+        import copy
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        for k, v in self.__dict__.items():
+            setattr(result, k, copy.deepcopy(v, memo))
+        return result
+
     def __getstate__(self):
         return self.optim.__getstate__()
 
